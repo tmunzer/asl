295b9a36b8e60c6b3ba73e983668d6cdadd4c354
tmunzer <tmunzer@aerohive.com>
2016-08-26 17:33:10 +0200

    mongo location

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     // Create your schemas and models here.
 });
-
-mongoose.connect('mongodb://localhost/asl');
+const mongodb = "mongo";
+mongoose.connect('mongodb://'+mongodb+'/asl');
 
 
 
