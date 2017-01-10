bdd0f8eac94f1952c65d2c31cce3d5ac4be6e652
Thomas Munzer <thomas@mbp-tmunzer.local>
2017-01-10 12:27:56 +0100

    rollback test

--- public/web-app/login.js
+++ public/web-app/login.js
@@ -2,6 +2,28 @@ var login = angular.module('login', [
     "ngRoute", 'ngSanitize', 'ngMaterial', 'pascalprecht.translate'
 ]);
 
+login
+    .config(function ($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette("blue")
+            .accentPalette('green', {
+                'default': '400' // by default use shade 400 from the pink palette for primary intentions
+            });
+    }).config(function ($translateProvider) {
+        $translateProvider.useMissingTranslationHandlerLog();
+        $translateProvider
+            .translations('en', en)
+            .translations('fr', fr)
+            .registerAvailableLanguageKeys(['en', 'fr'], {
+                'en_*': 'en',
+                'fr_*': 'fr',
+                '*': 'en'
+            })
+            .determinePreferredLanguage()
+            .fallbackLanguage('en')
+            .useSanitizeValueStrategy('sanitize');
+
+    });
 
 login.controller('LoginCtrl', function ($scope) {
 
